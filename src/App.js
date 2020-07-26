4167a12ade7b6fcb8b8da1207dd70138743e35a3
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-07-25 23:38:07 -0500

    Change example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
-import UseStateExample from './useState/HookComponent';
-import UseStateClassExample from './useState/ClassComponent';
+// import UseStateClassExample from './useState/ClassComponent';
+import UseStateHookExample from './useState/HookComponent';
+
 
 function App() {
   return (
     <div className="App">
-        <UseStateClassExample />
+        <UseStateHookExample />
     </div>
   );
 }
