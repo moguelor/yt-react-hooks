cedabd22aa5ff4a7809c1a0d576fe9ae01cea290
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-08-08 18:52:12 -0500

    Updates

---
+++ precache-manifest.c7dff242bd0b48a250c08c0fcb431ac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cd0662cb091b1a4ed566c9373fcd69",
+    "url": "/yt-react-hooks/index.html"
+  },
+  {
+    "revision": "1d408da80c901c8fa4bf",
+    "url": "/yt-react-hooks/static/css/main.d1c89aee.chunk.css"
+  },
+  {
+    "revision": "4bbcf66ba19af2a23f67",
+    "url": "/yt-react-hooks/static/js/2.642c04ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yt-react-hooks/static/js/2.642c04ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d408da80c901c8fa4bf",
+    "url": "/yt-react-hooks/static/js/main.adfc8e7a.chunk.js"
+  },
+  {
+    "revision": "0282d2f60abf54790561",
+    "url": "/yt-react-hooks/static/js/runtime-main.4d1cac47.js"
+  }
+]);
