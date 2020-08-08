57325f8b2155cd93979a32cd72392361c0c13c64
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-08-08 18:51:37 -0500

    Added react router for navegation.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import './App.css';
-import UseStateHookExample from './useState/HookComponent';
-import UseEffectHookExample from './useEffect/HookComponent';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UseStateHookExample from "./useState/HookComponent";
+import UseEffectHookExample from "./useEffect/HookComponent";
 
 function App() {
   return (
     <div className="App">
-        {/* <UseStateHookExample /> */}
-        <UseEffectHookExample />
+      <Router>
+        <Switch>
+          <Route exact path="/yt-react-hooks">
+            <UseStateHookExample />
+          </Route>
+          <Route path="/yt-react-hooks/useState">
+            <UseStateHookExample />
+          </Route>
+          <Route path="/yt-react-hooks/useEffect">
+            <UseEffectHookExample />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
