6bdea896c55920f3ca8c8c71b3a99a10481f8f15
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-08-25 22:41:30 -0500

    Pre - using hook useReducer

--- src/useReducer/HookComponent.js
+++ src/useReducer/HookComponent.js
@@ -1,66 +1,33 @@
-import React, { useReducer, useMemo } from "react";
+import React from "react";
 import { Item, Footer, Card, Input, Title } from "./components";
-import reducer, { INITIAL_STATE, init } from "./reducer";
 import { objectToString } from "../utils";
-import {
-  handleChangeInput,
-  addItem,
-  resetInput,
-  toggleItem,
-  setActiveFilter,
-  clearCompleted,
-  resetState,
-} from "./actions";
 
 const HookComponent = () => {
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE, init);
-  const { text, items, activeFilter } = state;
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addItem());
-    dispatch(resetInput());
   };
 
-  const filteredData = () => {
-    const filterByStatus = items.filter((item) =>
-      state.activeFilter === "all"
-        ? true
-        : state.activeFilter === "active"
-        ? item.active
-        : !item.active
-    );
-
-    return text
-      ? filterByStatus.filter((item) => item.text.indexOf(text) > -1)
-      : filterByStatus;
-  };
+  const state = {};
 
   return (
     <>
       <Title>todos</Title>
-      <Card width={450}>
+      <Card width={400}>
         <form onSubmit={handleSubmit}>
           <Input
             placeholder="What are you thinking to do?"
-            onChange={(e) => dispatch(handleChangeInput(e.target.value))}
-            value={text}
+            onChange={() => {}}
           />
         </form>
-        {filteredData().map((item, i) => (
-          <Item
-            key={i}
-            text={item.text}
-            isActive={item.active}
-            handleClickItem={() => dispatch(toggleItem(item.id))}
-          />
-        ))}
+        <Item text={"Element 1"} isActive handleClickItem={() => {}} />
+        <Item text={"Element 2"} isActive={false} handleClickItem={() => {}} />
+        <Item text={"Element 3"} isActive handleClickItem={() => {}} />
         <Footer
-          totalItems={items.filter((item) => item.active).length}
-          handleClickFilter={(filter) => dispatch(setActiveFilter(filter))}
-          active={activeFilter}
-          handleClickClear={() => dispatch(clearCompleted())}
-          handleClickReset={() => dispatch(resetState())}
+          totalItems={0}
+          handleClickFilter={() => {}}
+          active={"all"}
+          handleClickClear={() => {}}
+          handleClickReset={() => {}}
         />
       </Card>
       <pre>{objectToString(state)}</pre>
