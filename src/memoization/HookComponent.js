c8d12cfbdfb83664d3ee257003b4db18300c2be1
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-10-22 15:25:51 -0500

    Initial example

--- src/memoization/HookComponent.js
+++ src/memoization/HookComponent.js
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useMemo,
-  Profiler,
-} from "react";
+import React, { useState, useEffect } from "react";
 import { API_URL } from "./constants";
 import LikeButton from "./components/LikeButton";
 import HateButton from "./components/HateButton";
@@ -29,59 +23,28 @@ const HookComponent = () => {
     setTerm(e.target.value);
   };
 
-  /** useMemo */
-  // const filteredData = useMemo(
-  //   () => (term ? data.filter((item) => item.title.indexOf(term) > -1) : data),
-  //   [term, data]
-  // );
-
   const filteredData = term
     ? data.filter((item) => item.title.indexOf(term) > -1)
     : data;
 
-  /********************/
-
-  /** useCallback */
-  // const handleClickButtonLove = useCallback(() => {
-  //   setLoveCounter(loveCounter + 1);
-  // }, [loveCounter]);
-
   const handleClickButtonLove = () => {
     setLoveCounter(loveCounter + 1);
   };
-  /****************/
-
-  // const handleClickButtonHate = useCallback(() => {
-  //   setHateCounter(hateCounter + 1);
-  // }, [hateCounter]);
 
   const handleClickButtonHate = () => {
     setHateCounter(hateCounter + 1);
   };
 
   return (
-    <Profiler
-      id="profiler"
-      onRender={(id, phase, actualDuration) => {
-        console.group(`Time on ${phase}`);
-        console.log("actualDuration", actualDuration);
-        console.groupEnd();
-      }}
-    >
-      <div>
-        <h3>
-          Posts{" "}
-          <LikeButton handleClick={handleClickButtonLove} times={loveCounter} /> {" "}
-          <HateButton handleClick={handleClickButtonHate} times={hateCounter} />
-        </h3>
-        <Input
-          handleChange={handleChange}
-          placeholder="Search..."
-          value={term}
-        />
-        <Post data={filteredData}/>
-      </div>
-    </Profiler>
+    <div>
+      <h3>
+        Posts{" "}
+        <LikeButton handleClick={handleClickButtonLove} times={loveCounter} />{" "}
+        <HateButton handleClick={handleClickButtonHate} times={hateCounter} />
+      </h3>
+      <Input handleChange={handleChange} placeholder="Search..." value={term} />
+      <Post data={filteredData} />
+    </div>
   );
 };
 
--- src/memoization/components/Posts.js
+++ src/memoization/components/Posts.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { Card, Button } from "../../components";
+import { Card } from "../../components";
 
-const Posts = ({ data, term }) => {
-
+const Posts = ({ data }) => {
   return (
     <div>
-      {data.map((post) => {
-        return (
-          <Card width={700} key={post.id}>
-            <h4>
-              {post.title.toUpperCase()}
-            </h4>
-            <p>{post.body}</p>
-          </Card>
-        );
-      })}
+      {data.map((post) => (
+        <Card width={700} key={post.id}>
+          <h4>{post.title.toUpperCase()}</h4>
+          <p>{post.body}</p>
+        </Card>
+      ))}
     </div>
   );
 };
 
-// export default React.memo(Posts);
 export default Posts;
